0b0d3771c2776da8417b525d047431d21f50dfac
Paulus34 <pauljairuscaberte34@gmail.com>
2025-03-30 20:20:03 +0800

    added routes for account management

--- routes/api.php
+++ routes/api.php
@@ -1,7 +1,9 @@
 <?php
 
 use App\Http\Controllers\Auth\AuthController;
-use App\Http\Controllers\User\UserAccountController;
+use App\Http\Controllers\User\Customer\CustomerAccountController;
+use App\Http\Controllers\User\Employee\EmployeeAccountController;
+use App\Http\Controllers\User\UserController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -15,12 +17,21 @@ Route::middleware(['auth:sanctum'])->group(function () {
     Route::prefix('/users')->group(function () {
         //Authenticated
         Route::get('/current', [AuthController::class, 'user']);
-        Route::post('/update', [UserAccountController::class, 'updateCurrent']);
-        Route::post('/update-password', [UserAccountController::class, 'updatePassword']);
-
-        //Accounts
-        Route::get('/', [UserAccountController::class, 'viewAccounts']);
+        Route::post('/update', [UserController::class, 'updateCurrent']);
+        Route::post('/update-password', [UserController::class, 'updatePassword']);
     });
 
+    //Employees
+    Route::prefix('/employees')->group(function () {
+        Route::get('', [EmployeeAccountController::class, 'getEmployees']);
+        Route::post('/add', [EmployeeAccountController::class, 'addUser']);
+        Route::post('/{userId}', [EmployeeAccountController::class, 'updateUser']);
+    });
 
+    //Customers
+    Route::prefix('/customers')->group(function () {
+        Route::get('', [CustomerAccountController::class, 'getCustomers']);
+        Route::post('/add', [CustomerAccountController::class, 'addUser']);
+        Route::post('/{userId}', [CustomerAccountController::class, 'updateUser']);
+    });
 });
