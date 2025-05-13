55016c5003cb48078f2ce9114c1d2fdef8719102
Paulus34 <pauljairuscaberte34@gmail.com>
2025-05-13 20:42:55 +0800

    initial implementation.

---
+++ app/Http/Controllers/WorkloadController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class WorkloadController extends BaseController
+{
+    public function assignWorkload(int $id, Request $request)
+    {
+        
+    }
+}
