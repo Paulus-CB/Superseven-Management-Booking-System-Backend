63c2637a096af312c077c8a84df512edd9ba2357
Paulus <pauljairuscaberte34@gmail.com>
2025-05-28 19:52:19 +0800

    updated workload resource.

--- app/Http/Resources/WorkloadResource.php
+++ app/Http/Resources/WorkloadResource.php
@@ -16,6 +16,8 @@ class WorkloadResource extends JsonResource
      */
     public function toArray(Request $request): array
     {
+        $user = auth()->user()->id ?? 0;
+
         return [
             'id' => $this->id,
             'booking_date' => Carbon::parse($this->booking_date)->format('F d, Y'),
@@ -23,10 +25,13 @@ class WorkloadResource extends JsonResource
             'customer_name ' => $this->customer->full_name,
             'booking_address' => $this->booking_address,
             'completion_date' => Carbon::parse($this->completion_date)->format('F d, Y'),
-            'deliverable_status' => Booking::DELIVERABLE_STATUS[$this->deliverable_status],
+            'booking_workload_status' => Booking::DELIVERABLE_STATUS[$this->deliverable_status],
+            $this->mergeWhen($request->route()->named('workload.detail', 'workload.update'), [
+                'employee_workload_status' => Booking::DELIVERABLE_STATUS[$this->employees()->wherePivot('user_id', $user)->pluck('workload_status')->first()] ?? null
+            ]),
             'link' => $this->link,
             'assigned_count' => $this->employees->count() ?? 0,
-            'assigned_employees' => UserResource::collection($this->employees),
+            'assigned_employees' => WorkloadEmployeeResource::collection($this->employees),
         ];
     }
 }
