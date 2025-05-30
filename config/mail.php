1dc5e6aff1754e431d21136a356bbd1b54be0117
Paulus <pauljairuscaberte34@gmail.com>
2025-05-30 14:17:20 +0800

    setup mail config.

--- config/mail.php
+++ config/mail.php
@@ -109,8 +109,8 @@ return [
     */
 
     'from' => [
-        'address' => env('MAIL_FROM_ADDRESS', 'hello@example.com'),
-        'name' => env('MAIL_FROM_NAME', 'Example'),
+        'address' => env('MAIL_FROM_ADDRESS', 'noreply@superseven.com'),
+        'name' => env('MAIL_FROM_NAME', 'Superseven Studio'),
     ],
 
 ];
