f1fc8e0ab3a0485a2eeb1ecf58d9b27455a0a70d
Paulus <pauljairuscaberte34@gmail.com>
2025-06-03 10:53:20 +0800

    testing mail layout.

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,21 @@
 <?php
 
+use App\Mail\Client\CompletedBooking;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::get('/mailable/admin/booking', function () {
+    $booking = App\Models\Booking::inRandomOrder()->first();
+
+    return new App\Mail\Admin\ReceivedBooking($booking, 'John Doe');
+});
+
+
+Route::get('/mailable/client/booking', function () {
+    $booking = App\Models\Booking::inRandomOrder()->first();
+
+    return new App\Mail\Client\CompletedBooking($booking);
 });
