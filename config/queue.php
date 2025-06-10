53ff8690e71cb4bca1700652f7e3776bb7a8925c
Paulus <pauljairuscaberte34@gmail.com>
2025-06-10 15:59:08 +0800

    updated configuration in queue config.

--- config/queue.php
+++ config/queue.php
@@ -37,10 +37,10 @@ return [
         'database' => [
             'driver' => 'database',
             'connection' => env('DB_QUEUE_CONNECTION'),
-            'table' => env('DB_QUEUE_TABLE', 'jobs'),
-            'queue' => env('DB_QUEUE', 'default'),
-            'retry_after' => (int) env('DB_QUEUE_RETRY_AFTER', 90),
-            'after_commit' => false,
+            'table' => 'jobs',
+            'queue' => 'default',
+            'retry_after' => 900,
+            'after_commit' => true,
         ],
 
         'beanstalkd' => [
